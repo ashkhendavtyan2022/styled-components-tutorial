f3e5a3ec2dca81b0112cbb60cde4601e3a40081f
ashkhendavtyan2022 <119871908+ashkhendavtyan2022@users.noreply.github.com>
2023-05-18 01:27:51 +0400

    Final

---
+++ react-app/src/components/Socialicons.js
@@ -0,0 +1,27 @@
+import { FaTwitter } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { StyledSocialIcons } from "./styles/SocialIcons.styled";
+
+
+export default function SocialIcons() {
+    return (
+        <StyledSocialIcons>
+            <li>
+                <a href='https://twitter.com'>
+                <FaTwitter />
+                </a>
+            </li>
+            <li>
+                <a href='https://facebook.com'>
+                <FaFacebook />
+                </a>
+            </li>
+            <li>
+                <a href='https://linkedin.com'>
+                <FaLinkedin />
+                </a>
+            </li>
+        </StyledSocialIcons>
+    )
+}
